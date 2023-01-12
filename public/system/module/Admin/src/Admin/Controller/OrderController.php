c7e303ee397d7bdd94f319b78a750744d472d124
Le Mien <lemien@Les-MacBook-Air.local>
2023-01-12 17:41:15 +0700

    Updated

--- public/system/module/Admin/src/Admin/Controller/OrderController.php
+++ public/system/module/Admin/src/Admin/Controller/OrderController.php
@@ -5,19 +5,69 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Zend\View\Model\JsonModel;
 class OrderController extends AbstractActionController{
-	
-	public function indexAction(){
+
+    const version = 2;
+    const appId = 73193;
+    const businessId = 109797;
+    const accessToken = 'pV0IWWhXrpiM9SWgeGT8kClOaAF7caGiBCyZ4c2iIc2M9dvJLDv9NUTVI2RB4QP29zoQncLxQ4IZdTbYglchdOeBlKF0ovVR5P5TyJ52RHL55IGVPRgMMc6Ll0WHzRjE0kmQILxj2x3dLAuLY6AU13';
+    const data = '{"page": "68"}';
+
+    public function indexAction(){
 		$this->layout('layout/admin');
-		$model = new \Admin\Model\Order();
-        $mapper = $this->getServiceLocator()->get('Admin\Model\OrderMapper');
-        $model = $mapper->getId(166);
-        $model->setStatus(date("s"));
-        $mapper->save($model);
-        die;
-		$mapper = $this->getServiceLocator()->get('Admin\Model\OrderMapper');
-		$model->exchangeArray((array)$this->getRequest()->getQuery());
-		$page = (int)$this->getRequest()->getQuery()->page ?: 1;
-		$results = $mapper->search($model, array($page, 30));
+
+//        $request = new Request();
+//        $request->getHeaders()->addHeaders(array(
+//            'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8'
+//        ));
+//        $someurl = '';
+//        $request->setUri($someurl);
+//        $request->setMethod('POST');
+//        $request->setPost(new Parameters(array('someparam' => $somevalue)));
+//
+//        $client = new Client();
+//        $response = $client->dispatch($request);
+//        $data = json_decode($response->getBody(), true);
+
+        $curl = curl_init();
+        $data = array(
+            'version' => self::version,
+            'appId' => self::appId,
+            'businessId' => self::businessId,
+            'accessToken' => self::accessToken
+        );
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://open.nhanh.vn/api/order/index',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $data,
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        print_r(json_decode($response, true));die;
+
+        echo $response;
+
+
+
+//		$model = new \Admin\Model\Order();
+//        $mapper = $this->getServiceLocator()->get('Admin\Model\OrderMapper');
+//        $model = $mapper->getId(166);
+//        $model->setStatus(date("s"));
+//        $mapper->save($model);
+//        die;
+//		$mapper = $this->getServiceLocator()->get('Admin\Model\OrderMapper');
+//		$model->exchangeArray((array)$this->getRequest()->getQuery());
+//		$page = (int)$this->getRequest()->getQuery()->page ?: 1;
+//		$results = $mapper->search($model, array($page, 30));
 		return new ViewModel(array(
 			'pages'=>$page,
 			'results'=>$results,
--- public/system/public/tp/blog/js/form.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by mienle on 5/27/17.
- */
-$(function () {
-    $('form').parsley();
-});
