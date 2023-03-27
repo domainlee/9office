0286fa9ec59bc51bcc3be7cfec27e674028106c7
admin <admin@Mien-Le-iMac.local>
2023-03-27 23:26:40 +0700

    updated

--- public/system/module/Admin/src/Admin/Controller/MaterialController.php
+++ public/system/module/Admin/src/Admin/Controller/MaterialController.php
@@ -38,7 +38,7 @@ class MaterialController extends AbstractActionController{
 //        $dataOption = $optionMapper->get($option);
 //        $dataOld = !empty($dataOption) ?  json_decode($dataOption->getData(), true):'';
 		$page = (int)$this->getRequest()->getQuery()->page ? : 1;
-		$results = $mapper->search($model, array($page,10));
+		$results = $mapper->search($model, array($page,50));
 
 		return new ViewModel(array(
 //			'fFilter' => $fFilter,
@@ -156,7 +156,7 @@ class MaterialController extends AbstractActionController{
 
 
         $page = (int)$this->getRequest()->getQuery()->page ? : 1;
-        $results = $mapper->search($model, array($page,10));
+        $results = $mapper->search($model, array($page,50));
 
         return new ViewModel(array(
             'results' => $results,
